06d6c937ca2460c0e56ff7946a447c869aa7450b
sercanaydemir <serca>
2024-01-27 11:46:21 +0300

    camera movement added

---
+++ Assets/Scripts/Player/CameraFollower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float smoothSpeed = 0.125f;
+        [SerializeField] private Vector3 offset;
+
+        private void Start()
+        {
+            offset = transform.position - target.position;
+        }
+        
+        private void FixedUpdate()
+        {
+            Vector3 desiredPosition = target.position + offset;
+            desiredPosition.y = transform.position.y;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed*Time.fixedDeltaTime);
+            transform.position = smoothedPosition;
+        }
+    }
+}
